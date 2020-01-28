00ed8966ae5341774f580dc67e08fb9ee5f77374
Alex-og <alexhaleta99@gmail.com>
2020-01-28 19:23:39 +0200

    origin

--- src/main/java/Button1.java
+++ src/main/java/Button1.java
@@ -9,9 +9,12 @@ public class Button1 {
 
     private void go() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click like you mean it");
-        frame.getContentPane().add(BorderLayout.EAST, button);
+        JButton button = new JButton("Click This!");
+        Font font = new Font("serif", Font.BOLD, 28);
+        button.setFont(font);
+        frame.getContentPane().add(BorderLayout.NORTH, button);
         frame.setSize(200, 200);
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
