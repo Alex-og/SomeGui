3b65f9558dc53dcb0179ce9813b0a2b70854a163
Alex-og <alexhaleta99@gmail.com>
2020-01-28 19:31:28 +0200

    origin

--- src/main/java/Button1.java
+++ src/main/java/Button1.java
@@ -9,11 +9,21 @@ public class Button1 {
 
     private void go() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("Click This!");
-        Font font = new Font("serif", Font.BOLD, 28);
-        button.setFont(font);
-        frame.getContentPane().add(BorderLayout.NORTH, button);
-        frame.setSize(200, 200);
+
+        JButton east = new JButton("East");
+        JButton west = new JButton("West");
+        JButton north = new JButton("North");
+        JButton south = new JButton("South");
+        JButton center = new JButton("Center");
+//        Font font = new Font("serif", Font.BOLD, 28);
+//        button.setFont(font);
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
+
+        frame.setSize(300, 300);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
